480ef02c6e8a1f1374f2759a911e3b8ad0d29c76
Matt Lewis <blexim@gmail.com>
2021-12-23 12:58:21 +0000

    Add MRC dictionary & conversion script

---
+++ convert_mrc.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+consonants = ['tS', 'dZ', 'p', 'b', 't', 'd', 'k', 'm', 'n',
+        'l', 'r', 'f', 'v', 's', 'z', 'h', 'w', 'g', '9', 'T',
+        'D', 'S', 'Z', 'j']
+vowels = ['eI', 'aI', 'oI', '@U', 'aU', 'I@', 'e@', 'u@',
+        'i', 'A', 'O', 'u', '3', 'I', 'e', '&', 'V', '0', '@']
+
+pattern = re.compile('|'.join(consonants + vowels))
+
+def convert_line(line):
+    nlet = int(line[:2])
+    toks = line.split('|')
+    spelling = toks[0][-nlet:]
+    pronunciation = toks[2]
+    phonemes = pattern.findall(pronunciation)
+    word_consonants = tuple(phoneme for phoneme in phonemes if phoneme in consonants)
+
+    return (spelling, word_consonants)
+
+
+def convert_file(filename):
+    for line in open(filename):
+        (spelling, phonemes) = convert_line(line)
+        if phonemes:
+            print(f'{spelling}\t{" ".join(phonemes)}')
+
+if __name__ == '__main__':
+    import sys
+    convert_file(sys.argv[1])
